84ee45d52ad2dec2be5d94e1b4b77ecacdd1be0f
RaulSantosDev <mrpucky27@gmail.com>
2025-03-23 14:38:32 -0600

    Installing Tailwind Css

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ function App() {
 
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <h1 className=" text-emerald-800 text-6xl"
+      >Hola Mundo</h1>
+      <h1 className=" text-emerald-800 text-2xl"
+      >Hola Mundo</h1>
     </>
   )
 }
