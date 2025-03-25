fb46416b5d2c0114c712648a6d6f9182f64d4e80
RaulSantosDev <mrpucky27@gmail.com>
2025-03-24 19:54:50 -0600

    Add header, main

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,23 @@ function App() {
 
   return (
     <>
-      <h1 className=" text-emerald-800 text-6xl"
-      >Hola Mundo</h1>
-      <h1 className=" text-emerald-800 text-2xl"
-      >Hola Mundo</h1>
+      <header className="bg-emerald-400 py-5">
+        <h1 className="text-center text-4xl font-black ">Calculadora de propinas</h1>
+      </header>
+
+      <main className="max-w7-xl mx-auto py-5 grid md:grid-cols-2">
+        <div className="p-5">
+          <h1 className="text-center font-black text-4xl ">Main</h1>          
+        </div>  
+
+        <div>
+          <h1 className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">Main</h1>
+        </div>
+
+
+      </main>
+
+      
     </>
   )
 }
