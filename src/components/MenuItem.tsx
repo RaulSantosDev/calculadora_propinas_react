f4313fe06981ef57b460c707d4c9bcb75e870fa3
RaulSantosDev <mrpucky27@gmail.com>
2025-03-27 23:14:11 -0600

    Conecting our hook with the button

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -2,12 +2,16 @@ import { MenuItem } from "../types"
 
 type MenuItemProps = {
  item: MenuItem
+ addItem: () => void
 }
 
-export default function MuneItem({item} : MenuItemProps){
+export default function MuneItem({item, addItem} : MenuItemProps){
   return (
            
-    <button className="cursor-pointer border-2 border-teal-400 w-full p-3 flex justify-between hover:bg-teal-200 rounded-md ">
+    <button 
+      className="cursor-pointer border-2 border-teal-400 w-full p-3 flex justify-between hover:bg-teal-200 rounded-md "
+      onClick={() => addItem()}
+    >
       <p>{item.name}</p>
       <p className="font-black">${item.price}</p>
     </button>  
